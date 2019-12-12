7acab17c852b3e4d672d302fff6c8c8bd10f537e
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2019-12-12 22:38:15 +0900

    不要な行をコメントアウト

--- unittest/src/test/java/unittest/web/RootPageWithJetty.java
+++ unittest/src/test/java/unittest/web/RootPageWithJetty.java
@@ -3,24 +3,20 @@ package unittest.web;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.plus.webapp.EnvConfiguration;
-import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.Configuration;
-import org.eclipse.jetty.webapp.FragmentConfiguration;
-import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
-import org.eclipse.jetty.webapp.MetaInfConfiguration;
-import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.webapp.WebInfConfiguration;
 import org.eclipse.jetty.webapp.WebXmlConfiguration;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import unittest.servlet.IndexServlet;
@@ -29,38 +25,49 @@ import unittest.servlet.Initializer;
 public class RootPageWithJetty {
 	@Test
 	public void testLoginFormExists() throws Throwable{
-		Server s = new Server(8080);
+		Document doc = parse("/");
+		Elements elements = doc.select("form input");
+		assertTrue(elements.size() > 0, "フォームはinputダグを含んでいる。");
+		assertEquals("username", elements.get(0).attr("name"), "最初のinputタグのnameはusername");
+		assertEquals("password", elements.get(1).attr("name"), "2番目のinputタグのnameはpassword");
+	}
+
+	private Document parse(String path) throws MalformedURLException, IOException {
+		return Jsoup.parse(new URL("http://localhost:8080/unittest" + path), 5000);
+	}
+
+	private static Server server;
+
+	@BeforeAll
+	public static void setUp() throws Throwable{
+		server = new Server(8080);
 		WebAppContext wac = new WebAppContext();
-		File temp = new File("temp");
-		temp.mkdirs();
-		wac.setTempDirectory(temp);
 		wac.setContextPath("/unittest");
 		wac.setResourceBase("src/main/webapp");
-		wac.setParentLoaderPriority(true);
 		wac.setDescriptor("src/main/webapp/WEB-INF/web.xml");
-		Configuration[] configurations = {
+		wac.setConfigurations(new Configuration[]{
 				new AnnotationConfiguration(),
-				new WebInfConfiguration(),
-				new WebXmlConfiguration(),
-				new MetaInfConfiguration(),
-				new FragmentConfiguration(),
-				new EnvConfiguration(),
-				new PlusConfiguration(),
-				new JettyWebXmlConfiguration()
-		};
-		wac.setConfigurations(configurations);
+				new WebXmlConfiguration()
+		});
 		wac.addEventListener(new Initializer());
 		wac.addServlet(IndexServlet.class, "/");
-		s.setHandler(wac);
-		s.start();
-		try {
-			Document doc = Jsoup.parse(new URL("http://localhost:8080/unittest/"), 5000);
-			Elements elements = doc.select("form input");
-			assertTrue(elements.size() > 0, "フォームはinputダグを含んでいる。");
-			assertEquals("username", elements.get(0).attr("name"), "最初のinputタグのnameはusername");
-			assertEquals("password", elements.get(1).attr("name"), "2番目のinputタグのnameはpassword");
-		} finally {
-			s.stop();
-		}
-}
+/* 以下，自動でサーブレットやListenerをスキャンさせたいが動作しない
+		URL classes = IndexServlet.class.getProtectionDomain().getCodeSource().getLocation();
+		wac.getMetaData().setWebInfClassesDirs(Arrays.asList(Resource.newResource("target/classes"), Resource.newResource(classes)));
+		wac.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", 
+				".*target/classes/.*");
+		wac.setParentLoaderPriority(false);
+		WebAppClassLoader wcl = new WebAppClassLoader(wac);
+		wcl.addClassPath("target/classes");
+		wcl.addClassPath("src/test/webapp/WEB-INF/classes");
+		wac.setClassLoader(wcl);
+*/
+		server.setHandler(wac);
+		server.start();
+	}
+
+	@AfterAll
+	public static void tearDown() throws Throwable{
+		server.stop();
+	}
 }
